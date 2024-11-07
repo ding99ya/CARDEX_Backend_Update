167da57e0c52753704bd3af89878c665cccab7af
ding99ya <102636654+ding99ya@users.noreply.github.com>
2024-11-06 22:18:14 -0600

    Update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "dailyUpdateScore",
       script: "/home/ec2-user/CARDEX_Backend_Update/dailyUpdateScore.js",
-      cron_restart: "* * * * *",
+      cron_restart: "0 0 * * *",
       env: {
         TZ: "America/Chicago",
       },
